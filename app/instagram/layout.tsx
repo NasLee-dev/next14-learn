af68ebc98a9b2a77722e0d9961835fc720a1e395
wootaiklee <fined000@naver.com>
2024-08-20 20:51:18 +0900

    feat: instagram clone 셋팅

---
+++ app/instagram/layout.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { ThemeProvider } from "config/material-tailwind-theme-provider";
+import ReactQueryClientProvider from "config/ReactQueryClientProvider";
+import RecoilProvider from "config/RecoilProvider";
+
+export default function RootLayout({ children }) {
+  return (
+    <RecoilProvider>
+      <ReactQueryClientProvider>
+        <ThemeProvider>
+            {/* @ts-ignore */}
+            <html lang="en">
+              <head>
+                <link
+                  rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+                  integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+                  crossOrigin="anonymous"
+                  referrerPolicy="no-referrer"
+                  />
+              </head>
+              <body>
+                {children}
+              </body>
+            </html>
+          </ThemeProvider>
+      </ReactQueryClientProvider>
+    </RecoilProvider>
+  );
+}
---
+++ app/instagram/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Instagram",
+  description: "Instagram Page",
+}
+
+export default function InstagramPage() {
+  return (
+    <main>
+      <h1>Instagram Page</h1>
+    </main>
+  );
+}
